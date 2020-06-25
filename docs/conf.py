0c78f60a93ea9a851cf78aea84b5fe457a50b999
Sorin Sbarnea <ssbarnea@redhat.com>
2020-06-25 14:49:43 +0100

    Allow custom title

--- docs/conf.py
+++ docs/conf.py
@@ -125,7 +125,7 @@ rst_epilog = """
 
 html_theme_path = [sphinx_ansible_theme.get_html_theme_path()]
 html_theme = 'sphinx_ansible_theme'
-html_short_title = 'Ansible Documentation'
+html_short_title = f'{project} Documentation'
 html_show_sphinx = False
 
 html_theme_options = {
@@ -156,7 +156,7 @@ html_context = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Ansible Documentation'
+html_title = f'{project} Documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
