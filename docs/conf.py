d81e1ef25dc74eb4ba04b98bfc2925cce6af25a4
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-04-29 19:33:15 +0200

    Mv extra CSS example to `html_css_files`

--- docs/conf.py
+++ docs/conf.py
@@ -149,15 +149,17 @@ html_context = {
     'latest_version': 'latest',
     # list specifically out of order to make latest work
     'available_versions': ('latest', ),
-    'css_files': [  # overrides to the standard theme
-        '_static/css/color-scheme.css',
-    ],
 }
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
 # html_style = 'solar.css'
+
+# Add extra CSS styles to the resulting HTML pages
+html_css_files = [
+    'css/color-scheme.css',
+]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
