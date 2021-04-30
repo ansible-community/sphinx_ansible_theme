4612ca2681f4f1f84578a855985e7d543c1d016d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2021-04-30 19:15:36 +0200

    Integrate logo&favicon fallbacks

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,15 @@ html_title = f'{project} Documentation'
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = 'Documentation'
 
+# The name of an image file (within the static path) to place at the top of
+# the sidebar.
+html_logo = '_static/images/logo_invert.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '_static/favicon.ico'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
